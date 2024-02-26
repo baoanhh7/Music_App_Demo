8fe75b85c47b22d57f488913dbdab3a71a6b3c5a
baoanhh7 <namle0442@gmail.com>
2024-02-27 02:30:31 +0700

    final commit 26/2

--- app/src/main/java/com/example/doan_music/MainActivity.java
+++ app/src/main/java/com/example/doan_music/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
         view_pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
 
             @Override
