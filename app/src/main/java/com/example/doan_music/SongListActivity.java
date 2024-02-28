37c9d9a5b098ef186c1fd4346e56ba95ec763025
baoanhh7 <namle0442@gmail.com>
2024-02-28 23:34:28 +0700

    final commit 28/2

--- app/src/main/java/com/example/doan_music/SongListActivity.java
+++ app/src/main/java/com/example/doan_music/SongListActivity.java
@@ -1,14 +1,52 @@
 package com.example.doan_music;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.doan_music.adapter.SearchAdapter;
+import com.example.doan_music.adapter.SongListAdapter;
+import com.example.doan_music.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SongListActivity extends AppCompatActivity {
+
+    RecyclerView rcv_songlist;
+    SongListAdapter songListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
+
+        addControls();
+
+        songListAdapter.setData(getList());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        rcv_songlist.setLayoutManager(linearLayoutManager);
+
+    }
+
+    private List<User> getList() {
+        List<User> list = new ArrayList<>();
+        list.add(new User(R.drawable.avt_vu,"Radio.",true));
+        list.add(new User(R.drawable.avt_vu,"Radio.",true));
+        list.add(new User(R.drawable.avt_vu,"Radio.",true));
+        list.add(new User(R.drawable.avt_vu,"Radio.",true));
+        list.add(new User(R.drawable.avt_vu,"Radio.",true));
+        list.add(new User(R.drawable.avt_vu,"Radio.",true));
+
+        return list;
+    }
+
+    private void addControls() {
+        rcv_songlist = findViewById(R.id.rcv_songlist);
+        songListAdapter = new SongListAdapter();
+        rcv_songlist.setAdapter(songListAdapter);
     }
 }
---
+++ app/src/main/java/com/example/doan_music/adapter/SongListAdapter.java
@@ -0,0 +1,60 @@
+package com.example.doan_music.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.doan_music.R;
+import com.example.doan_music.SongListActivity;
+import com.example.doan_music.model.User;
+
+import java.util.List;
+
+public class SongListAdapter extends RecyclerView.Adapter<SongListAdapter.UserViewHolder> {
+
+    List<User> list;
+
+    public void setData(List<User> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_songlist, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = list.get(position);
+
+        holder.img_sList.setImageResource(user.getResourceImage());
+        holder.txt_sList.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list != null) return list.size();
+        return 0;
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView img_sList;
+        TextView txt_sList;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            img_sList = itemView.findViewById(R.id.img_songlist);
+            txt_sList = itemView.findViewById(R.id.txt_songlist);
+        }
+    }
+}
