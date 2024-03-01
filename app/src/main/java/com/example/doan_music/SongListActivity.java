4c74520ea87953e8ab36eec5cf42510997cd438a
baoanhh7 <namle0442@gmail.com>
2024-03-01 11:18:14 +0700

    final commit 1/3

--- app/src/main/java/com/example/doan_music/SongListActivity.java
+++ app/src/main/java/com/example/doan_music/SongListActivity.java
@@ -55,11 +55,11 @@ public class SongListActivity extends AppCompatActivity {
     private List<User> getList() {
         List<User> list = new ArrayList<>();
         list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio.", true));
-        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio..", true));
-        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio..", true));
-        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio..", true));
-        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio..", true));
-        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio..", true));
+        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio.", true));
+        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio.", true));
+        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio.", true));
+        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio.", true));
+        list.add(new User(R.drawable.avt_vu, "Những lời hứa bỏ quên - Vũ Radio.", true));
 
         return list;
     }
