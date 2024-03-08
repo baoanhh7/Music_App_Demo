004e8879353545cade00ded2708420d82fe27e8c
baoanhh7 <namle0442@gmail.com>
2024-03-08 14:46:54 +0700

    BA commit 8/3 2:46 PM

---
+++ app/src/main/java/com/example/doan_music/adapter/playlist/PlayListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.doan_music.adapter.playlist;
+
+public class PlayListAdapter {
+}
--- app/src/main/java/com/example/doan_music/model/Playlists.java
+++ app/src/main/java/com/example/doan_music/model/Playlists.java
@@ -6,4 +6,34 @@ public class Playlists implements Serializable {
     private int PlaylistID;
     private String PlaylistName;
     private byte[] PlaylistImage;
+
+    public Playlists(int playlistID, String playlistName, byte[] playlistImage) {
+        PlaylistID = playlistID;
+        PlaylistName = playlistName;
+        PlaylistImage = playlistImage;
+    }
+
+    public int getPlaylistID() {
+        return PlaylistID;
+    }
+
+    public void setPlaylistID(int playlistID) {
+        PlaylistID = playlistID;
+    }
+
+    public String getPlaylistName() {
+        return PlaylistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        PlaylistName = playlistName;
+    }
+
+    public byte[] getPlaylistImage() {
+        return PlaylistImage;
+    }
+
+    public void setPlaylistImage(byte[] playlistImage) {
+        PlaylistImage = playlistImage;
+    }
 }
