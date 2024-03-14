a6474923a3aa1f28da77324295e7a30167bd6732
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-03-15 00:17:29 +0700

    Khang update 14/3   2:58 a.m

--- app/src/main/java/com/example/doan_music/music/PlayMusicActivity.java
+++ app/src/main/java/com/example/doan_music/music/PlayMusicActivity.java
@@ -178,6 +178,9 @@ public class PlayMusicActivity extends AppCompatActivity {
                     }
                 }
                 cursor.close();
+                String duration = timeSeekbar(myMusic.getDuration());
+                txt_time.setText(duration);
+                seekBar.setMax(myMusic.getDuration());
                 myMusic.start();
                 database = openOrCreateDatabase("doanmusic.db", MODE_PRIVATE, null);
                 Cursor cursor1 = database.rawQuery("select * " +
@@ -230,6 +233,9 @@ public class PlayMusicActivity extends AppCompatActivity {
                     }
                 }
                 cursor.close();
+                String duration = timeSeekbar(myMusic.getDuration());
+                txt_time.setText(duration);
+                seekBar.setMax(myMusic.getDuration());
                 myMusic.start();
                 database = openOrCreateDatabase("doanmusic.db", MODE_PRIVATE, null);
                 Cursor cursor1 = database.rawQuery("select * " +
