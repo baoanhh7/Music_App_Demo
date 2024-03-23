65d9f12651427bb43e0d6ca4a77ff84567a20e4a
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-03-23 14:48:48 +0700

    Khang update 23/3    2:48p.m

--- app/src/main/java/com/example/doan_music/activity/admin/artist/AddArtistActivity.java
+++ app/src/main/java/com/example/doan_music/activity/admin/artist/AddArtistActivity.java
@@ -1,26 +1,192 @@
 package com.example.doan_music.activity.admin.artist;
 
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.doan_music.R;
+import com.example.doan_music.activity.admin.album.AddAlbumActivity;
+import com.example.doan_music.activity.admin.album.AlbumActivity;
+import com.example.doan_music.data.DatabaseManager;
+import com.example.doan_music.data.DbHelper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddArtistActivity extends AppCompatActivity {
-
+    EditText edtMa, edtTen;
+    DbHelper dbHelper;
+    Button btnSave, btncancel, btn_choose_image_addArtistAdmin;
+    ImageButton btn_camera;
+    SQLiteDatabase database = null;
+    ImageView imageView;
+    Spinner sp_id_artistadmin;
+    List<Integer> listIDArtist = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_artist);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        addControls();
+        createDataSpinner();
+        addEvents();
+    }
+
+    private void createDataSpinner() {
+        dbHelper = DatabaseManager.dbHelper(this);
+        database = dbHelper.getReadableDatabase();
+
+        // Artist
+
+
+        Cursor cursor = database.rawQuery("select * from Artists", null);
+        while (cursor.moveToNext()) {
+            Integer id = cursor.getInt(0);
+
+            listIDArtist.add(id);
+        }
+        cursor.close();
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listIDArtist);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_id_artistadmin.setAdapter(adapter);
+    }
+
+    private void addEvents() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                byte[] anh = getByteArrayFromImageView(imageView);
+                Integer id1 = Integer.valueOf(edtMa.getText().toString().trim());
+                Boolean Isid = false;
+                for (int i = 0; i < listIDArtist.size(); i++) {
+                    if (id1 == listIDArtist.get(i)) {
+                        Isid = false;
+                    } else
+                        Isid = true;
+                }
+                if (Isid) {
+                    ContentValues values = new ContentValues();
+                    values.put("ArtistID", edtMa.getText().toString() + "");
+                    values.put("ArtistName", edtTen.getText().toString());
+                    values.put("ArtistImage", anh);
+                    dbHelper = DatabaseManager.dbHelper(AddArtistActivity.this);
+                    long kq = dbHelper.getReadableDatabase().insert("Artists", null, values);
+                    if (kq > 0) {
+                        Toast.makeText(AddArtistActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else
+                        Toast.makeText(AddArtistActivity.this, "Thêm thất bại", Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(AddArtistActivity.this, "Id Artist đã có", Toast.LENGTH_SHORT).show();
+            }
         });
+        btncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtMa.setText("");
+                edtTen.setText("");
+                startActivity(new Intent(AddArtistActivity.this, ArtistActivity.class));
+            }
+        });
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+        btn_choose_image_addArtistAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePhoto();
+            }
+        });
+    }
+
+    private void addControls() {
+        edtMa = findViewById(R.id.edt_id_artistadmin);
+        edtTen = findViewById(R.id.edt_name_artistadmin);
+        imageView = findViewById(R.id.img_addArtistAdmin);
+        btnSave = findViewById(R.id.btn_save_artistadmin);
+        btncancel = findViewById(R.id.btn_cancel_artistadmin);
+        btn_choose_image_addArtistAdmin = findViewById(R.id.btn_choose_image_addArtistAdmin);
+        btn_camera = findViewById(R.id.btn_camera_artistadmin);
+        sp_id_artistadmin = findViewById(R.id.sp_id_artistadmin);
+    }
+    private byte[] getByteArrayFromImageView(ImageView img) {
+        BitmapDrawable drawable = (BitmapDrawable) img.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(ACTION_IMAGE_CAPTURE);
+        if (ActivityCompat.checkSelfPermission(AddArtistActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(AddArtistActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+            return;
+        }
+        //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        startActivityForResult(takePictureIntent, 99);
+        //}
+    }
+
+    private void choosePhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 99 && resultCode == Activity.RESULT_OK) {
+            // Xử lý ảnh đã chụp tại đây (nếu cần)
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // Hiển thị ảnh hoặc thực hiện các xử lý khác theo nhu cầu của bạn
+            imageView.setImageBitmap(imageBitmap);
+        } else if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            try {
+                Uri imageUri = data.getData();
+                InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
